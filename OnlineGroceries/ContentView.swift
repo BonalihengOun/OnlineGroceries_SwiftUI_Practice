2e80667edcbe181c9816935d84e7095afe167b8b
BonalihengOun <bonalyhengun@gmail.com>
2024-07-16 22:32:26 +0700

    Initial Commit

---
+++ OnlineGroceries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineGroceries
+//
+//  Created by Ounbonaliheng on 16/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by Ounbonaliheng on 16/7/24.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
